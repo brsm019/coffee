4a44789181ffb3fe22a2d48dc9ac6ffaa01447d6
Bradley Smith <bradleysmith@Bs-Macbook.local>
2021-06-25 07:12:27 +0100

    CSS sizing correct on home screen

--- src/CardLarge.js
+++ src/CardLarge.js
@@ -14,33 +14,37 @@ const CardLarge = () => {
           }}
         ></div> */}
         <img
-          src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(73).jpg"
+          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdenverbeverage.com%2Fwp-content%2Fuploads%2F2019%2F03%2FMocha_Cappuccino_2048x.jpg&f=1&nofb=1"
           class="w-100 h-100 shadow-1-strong rounded mb-4"
           id="cardlarge__image"
           alt=""
         />
       </div>
 
-      <div class="col-lg-3 col-md-12 col-sm-12 mb-4 mb-lg-0">
+      <div class="col-lg-3 col-md-6 col-sm-6 mb-4 mb-lg-0">
         <img
+          id="cardlarge__small"
           src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg"
           class="w-100 h-50 shadow-1-strong rounded mb-4"
           alt=""
         />
 
         <img
+          id="cardlarge__small"
           src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg"
           class="w-100 shadow-1-strong rounded mb-4"
           alt=""
         />
       </div>
-      <div class="col-lg-3 col-md-12 col-sm-12 mb-4 mb-lg-0">
+      <div class="col-lg-3 col-md-6 col-sm-6 mb-4 mb-lg-0">
         <img
+          id="cardlarge__small"
           src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg"
           class="w-100 h-50 shadow-1-strong rounded mb-4"
           alt=""
         />
         <img
+          id="cardlarge__small"
           src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg"
           class="w-100 shadow-1-strong rounded mb-4"
           alt=""
