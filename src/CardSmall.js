999a8a799beaffac459ab255ad381b895b88e76f
Bradley Smith <bradleysmith@Bs-Macbook.local>
2021-06-25 07:12:27 +0100

    CSS sizing correct on home screen

--- src/CardSmall.js
+++ src/CardSmall.js
@@ -3,28 +3,28 @@ import "./CardSmall.css";
 const CardSmall = () => {
   return (
     <>
-      <div class="col-lg-3 col-md-12 col-sm-12 mb-4 mb-lg-0">
+      <div class="col-lg-3 col-md-6 col-sm-6 mb-4 mb-lg-0">
         <img
           src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg"
           class="w-100 shadow-1-strong rounded mb-4 mt-4"
           alt=""
         />
       </div>
-      <div class="col-lg-3 col-md-12 col-sm-12 mb-4 mb-lg-0">
+      <div class="col-lg-3 col-md-6 col-sm-6 mb-4 mb-lg-0">
         <img
           src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg"
           class="w-100 shadow-1-strong rounded mb-4 mt-4"
           alt=""
         />
       </div>
-      <div class="col-lg-3 col-md-12 col-sm-12 mb-4 mb-lg-0">
+      <div class="col-lg-3 col-md-6 col-sm-6 mb-4 mb-lg-0">
         <img
           src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg"
           class="w-100 shadow-1-strong rounded mb-4 mt-4"
           alt=""
         />
       </div>
-      <div class="col-lg-3 col-md-12 col-sm-12 mb-4 mb-lg-0">
+      <div class="col-lg-3 col-md-6 col-sm-6 mb-4 mb-lg-0">
         <img
           src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg"
           class="w-100 shadow-1-strong rounded mb-4 mt-4"
