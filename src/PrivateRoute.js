4d5f714498c98c4193b7c8b6437d44de0928780c
Bradley Smith <bradleysmith@Bs-Macbook.local>
2021-06-30 08:18:09 +0100

    Working on PrivateRoute

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import { isLogin } from "../utils";
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
+function PrivateRoute({ children, ...rest }) {
+  let auth = localStorage.getItem("auth") === "true";
   return (
-    // Show the component only when the user is logged in
-    // Otherwise, redirect the user to /signin page
     <Route
       {...rest}
-      render={(props) =>
-        isLogin() ? <Component {...props} /> : <Redirect to="/SignUp" />
+      render={({ location }) =>
+        auth.user ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
       }
     />
   );
-};
+}
 
 export default PrivateRoute;
