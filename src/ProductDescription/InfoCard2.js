980d8cdffe13ad2ed1c18e8254f6a12ec958cfa0
Bradley Smith <bradleysmith@Bs-Macbook.local>
2021-07-03 14:12:59 +0100

    All coffee pages created

--- src/ProductDescription/InfoCard2.js
+++ src/ProductDescription/InfoCard2.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./InfoCard2.css";
 
-const InfoCard2 = ({ preTitle, title1, title2, title3, info }) => {
+const InfoCard2 = ({ preTitle, title9, title10, title11, info }) => {
   return (
     <div class="infoCard2__container">
       <div class="infoCard2__wrapper">
         <p class="infoCard2__pretitle">{preTitle}</p>
         <h1 class="infoCard2__title">
-          {title1} <span class="infoCard2__title__span">/</span> {title2}{" "}
-          <span class="infoCard2__title__span">/</span> {title3}
+          {title9} <span class="infoCard2__title__span">/</span> {title10}{" "}
+          <span class="infoCard2__title__span">/</span> {title11}
         </h1>
         <p class="infoCard2__description">{info}</p>
       </div>
--- src/ProductDescription/PictureCard.js
+++ src/ProductDescription/PictureCard.js
@@ -4,11 +4,7 @@ import "./PictureCard.css";
 const PictureCard = ({ image }) => {
   return (
     <>
-      <img
-        class="pictureCard__container"
-        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdenverbeverage.com%2Fwp-content%2Fuploads%2F2019%2F03%2FMocha_Cappuccino_2048x.jpg&f=1&nofb=1"
-        alt=""
-      />
+      <img class="pictureCard__container" src={image} alt="" />
     </>
   );
 };
