12f572f951ae2612c25f76b95fc31140a83e9b61
Bradley Smith <bradleysmith@Bs-Macbook.local>
2021-08-03 21:45:49 +0100

    h try5

--- backend/api/server.js
+++ backend/api/server.js
@@ -69,7 +69,7 @@ app.use(function (req, res) {
   res.status(404).send({ url: req.originalUrl + " not found" });
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log(`Listening to port ${port}`);
 });
 
