ddcd41ee73d8214bedc5f0ca0f5cba30d04e6e1f
Bradley Smith <bradleysmith@Bs-Macbook.local>
2021-08-05 08:08:55 +0100

    heroku added to backend

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -21,7 +21,7 @@ const Login = () => {
   };
   const handleLogin = (e) => {
     e.preventDefault();
-    const postURL = "/auth/sign_in"; //Our previously set up route in the backends
+    const postURL = "/auth/sign_in"; //Our previously set up route in the backend
     fetch(postURL, {
       method: "POST",
       headers: {
