d109afa1b6d8ddb529eca283ec6565c37d04f79f
Bradley Smith <brsm019@gmail.com>
2021-09-20 17:13:33 +0100

    Test for every link on homepage

--- __tests__/index.js
+++ __tests__/index.js
@@ -11,6 +11,7 @@ let opts = new chrome.Options();
     .forBrowser("chrome")
     // .setChromeOptions(opts.headless())
     .build();
+  await driver.manage().window().maximize();
   await driver.get("http://localhost:3000/");
 
   await driver.wait(
@@ -24,7 +25,7 @@ let opts = new chrome.Options();
     500
   );
 
-  async function urlTest(url1, xpath) {
+  async function urlTest(url1, xpath, commonXpath) {
     try {
       console.log({ url1 });
       await driver.wait(
@@ -54,13 +55,11 @@ let opts = new chrome.Options();
         }
       });
       const newWindow = await driver.getWindowHandle();
+      console.log({ originalWindow });
+      console.log({ newWindow });
 
       await driver.wait(
-        until.elementLocated(
-          By.xpath(
-            "/html/body/div/div/div/div[2]/div[2]/div[1]/div/div/div[4]/button"
-          )
-        ),
+        until.elementLocated(By.xpath(commonXpath)),
         10000,
         "Timed out after 10s",
         500
@@ -80,68 +79,158 @@ let opts = new chrome.Options();
     /* 
     
         Tests for homepage:
-        Test if each card navigates to correct page
+        Test all links on homepage go to right destination
    
    */
-    //Don't change position of these
-    const actualUrl = [
-      "http://localhost:3000/redbrick",
-      "http://localhost:3000/sweet-shop",
-      "http://localhost:3000/decaf-espresso",
-      "http://localhost:3000/sasaba",
-      "http://localhost:3000/la-floresta",
-      "http://localhost:3000/adela-reyes",
-      "http://localhost:3000/filtered-bean",
-      "http://localhost:3000/el-diamante",
-      "http://localhost:3000/dis-palo",
-      "http://localhost:3000/smart-dripper",
-      "http://localhost:3000/bookkisa",
-      "http://localhost:3000/el-dasto",
-      "http://localhost:3000/peretu",
-      "http://localhost:3000/lultrina",
+
+    //Homepage data
+    const homeData = [
+      {
+        actualUrl: "http://localhost:3000/",
+        xpath: "/html/body/div/div/div/div[1]/ul/li[1]/a",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div/div/div[2]/div[2]/div[1]/div[1]/div/h3",
+      },
+      {
+        actualUrl: "http://localhost:3000/equipment",
+        xpath: "/html/body/div/div/div/div[1]/ul/li[2]/a",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div/div/div[2]/div[2]/div[1]/div[1]/div/h3",
+      },
+      {
+        actualUrl: "http://localhost:3000/login", //When unauthenticated
+        xpath: "/html/body/div/div/div/div[1]/ul/li[3]/a",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div/div/div[3]/div[1]/div[1]/div/h3",
+      },
+      {
+        actualUrl: "http://localhost:3000/signup",
+        xpath: "/html/body/div/div/div/div[2]/div/div/div[1]/a[1]",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div/div[2]/div[2]/div[1]/div[1]/div/h3",
+      },
+      {
+        actualUrl: "http://localhost:3000/login",
+        xpath: "/html/body/div/div/div/div[2]/div/div/div[1]/a[2]",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div/div/div[3]/div[1]/div[1]/div/h3",
+      },
+      {
+        actualUrl: "http://localhost:3000/login", //When unauthenticated
+        xpath: "/html/body/div/div/div/div[2]/div/div/div[1]/a[3]",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div/div/div[3]/div[1]/div[1]/div/h3",
+      },
+      {
+        actualUrl: "http://localhost:3000/redbrick",
+        xpath:
+          "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[1]/a/div/div",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div[2]/div[1]/div/div/div[4]/button",
+      },
+      {
+        actualUrl: "http://localhost:3000/sweet-shop",
+        xpath:
+          "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[2]/a/div/div",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div[2]/div[1]/div/div/div[4]/button",
+      },
+      {
+        actualUrl: "http://localhost:3000/decaf-espresso",
+        xpath:
+          "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[3]/a/div/div",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div[2]/div[1]/div/div/div[4]/button",
+      },
+      {
+        actualUrl: "http://localhost:3000/sasaba",
+        xpath:
+          "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[4]/a/div/div",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div[2]/div[1]/div/div/div[4]/button",
+      },
+      {
+        actualUrl: "http://localhost:3000/la-floresta",
+        xpath:
+          "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[5]/a/div/div",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div[2]/div[1]/div/div/div[4]/button",
+      },
+      {
+        actualUrl: "http://localhost:3000/adela-reyes",
+        xpath:
+          "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[6]/a/div/div",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div[2]/div[1]/div/div/div[4]/button",
+      },
+      {
+        actualUrl: "http://localhost:3000/filtered-bean",
+        xpath:
+          "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[7]/a/div/div",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div[2]/div[1]/div/div/div[4]/button",
+      },
+      {
+        actualUrl: "http://localhost:3000/el-diamante",
+        xpath:
+          "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[8]/a/div/div",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div[2]/div[1]/div/div/div[4]/button",
+      },
+      {
+        actualUrl: "http://localhost:3000/dis-palo",
+        xpath:
+          "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[9]/a/div/div",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div[2]/div[1]/div/div/div[4]/button",
+      },
+      {
+        actualUrl: "http://localhost:3000/smart-dripper",
+        xpath:
+          "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[10]/a/div/div",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div[2]/div[1]/div/div/div[4]/button",
+      },
+      {
+        actualUrl: "http://localhost:3000/bookkisa",
+        xpath:
+          "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[11]/a/div/div",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div[2]/div[1]/div/div/div[4]/button",
+      },
+      {
+        actualUrl: "http://localhost:3000/el-dasto",
+        xpath:
+          "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[12]/a/div/div",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div[2]/div[1]/div/div/div[4]/button",
+      },
+      {
+        actualUrl: "http://localhost:3000/peretu",
+        xpath:
+          "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[13]/a/div/div",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div[2]/div[1]/div/div/div[4]/button",
+      },
+      {
+        actualUrl: "http://localhost:3000/lultrina",
+        xpath:
+          "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[14]/a/div/div",
+        commonXpath:
+          "/html/body/div/div/div/div[2]/div[2]/div[1]/div/div/div[4]/button",
+      },
     ];
 
-    const xpath = [
-      "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[1]/a/div/div",
-      "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[2]/a/div/div",
-      "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[3]/a/div/div",
-      "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[4]/a/div/div",
-      "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[5]/a/div/div",
-      "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[6]/a/div/div",
-      "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[7]/a/div/div",
-      "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[8]/a/div/div",
-      "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[9]/a/div/div",
-      "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[10]/a/div/div",
-      "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[11]/a/div/div",
-      "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[12]/a/div/div",
-      "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[13]/a/div/div",
-      "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[14]/a/div/div",
-    ];
+    for (let i = 0; i < homeData.length; i++) {
+      await urlTest(
+        homeData[i].actualUrl,
+        homeData[i].xpath,
+        homeData[i].commonXpath
+      );
+    }
 
-    await urlTest(actualUrl[0], xpath[0]);
-    await urlTest(actualUrl[1], xpath[1]);
-    await urlTest(actualUrl[2], xpath[2]);
-    await urlTest(actualUrl[3], xpath[3]);
-    await urlTest(actualUrl[4], xpath[4]);
-    // let elDiamante = driver.findElement(
-    //   By.xpath(
-    //     "/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[8]/a/div/div"
-    //   )
-    // );
-    // const actions = driver.actions({ async: true });
-    // // Performs mouse move action onto the element
-    // await actions.move({ origin: elDiamante }).perform();
-    await urlTest(actualUrl[5], xpath[5]);
-    await urlTest(actualUrl[6], xpath[6]);
-
-    await urlTest(actualUrl[7], xpath[7]);
-    await urlTest(actualUrl[8], xpath[8]);
-    await urlTest(actualUrl[9], xpath[9]);
-    await urlTest(actualUrl[10], xpath[10]);
-    await urlTest(actualUrl[11], xpath[11]);
-    await urlTest(actualUrl[12], xpath[12]);
-    TODO:
     //Fix error:- MoveTargetOutOfBoundsError: move target out of bounds, selenium doesn't select elements of screen, need to create scroll functionality or something alike.
+    //Find x and y coordinates of the element you want to click and try clicking that way
     //code is currently working. Need to refactor, create a loop of some kind
   } catch (e) {
     console.log(e);
