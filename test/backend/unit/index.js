e8a16e3c23c3e931d54af55befcf525f3ee3a0c2
algoJS <brsm019@gmail.com>
2023-02-04 13:49:56 +0000

    Created an index for the tests

---
+++ test/backend/unit/index.js
@@ -0,0 +1,11 @@
+const checkoutTests = require("./checkout.test");
+const loginTests = require("./login.test");
+const passwordTests = require("./password.test");
+const signinTests = require("./signin.test");
+
+module.exports = {
+  checkoutTests,
+  loginTests,
+  passwordTests,
+  signinTests,
+};
